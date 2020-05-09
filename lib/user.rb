17fea8faf96ef60b4111bb50d060c95315c1c204
Jack Pincus <jwpincus@gmail.com>
2020-05-09 10:56:31 -0700

    passes user tests, creates user class

--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,7 @@ class User
     attr_reader :age, :sex, :nationality
 
     def initialize(age:, sex:, nationality:)
+        require 'pry'; binding.pry
         @age = age
         @sex = sex
         @nationality = nationality
